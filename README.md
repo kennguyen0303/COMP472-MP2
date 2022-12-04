64a08815faf5c2c212abab4dbf22ac0ac2b7630f
kennguyen0303 <kenn.tnguyen@gmail.com>
2022-12-04 14:17:14 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # COMP472-MP2
 
+Github URL: https://github.com/kennguyen0303/COMP472-MP2
+
+
+(Optional step: Create a virtual environment: python3 -m venv venv)
+
+
 Install requirements.txt
 
-Run main.py for the ... files 
+Run <src/main.py> for running the search algorithms against the sample-input.txt file. The output will be exported in /outputs/ 
 
-Run test.py for the excel sheet with the given 50 random puzzles
+Run <src/run_sample_puzzles.py> to execute all search algorithms on the 50 random puzzles. A <test.csv> file will be generated in /src/
+
+Running unit tests: 
+
+Step 1: Cd ~/COMP472-MP2
+Step 2: run: <python -m pytest tests/>
+
+All tests should pass.
--- tests/test_heuristic.py
+++ tests/test_heuristic.py
@@ -1,7 +1,12 @@
 """
 Module for tests for heuristic calculation
 """
-from search_algos import calculate_heuristic_1
+from search_algos import (
+    calculate_heuristic_1,
+    calculate_heuristic_2,
+    calculate_heuristic_3,
+    calculate_heuristic_4,
+)
 
 
 def test_heuristic_1():
@@ -15,3 +20,42 @@ def test_heuristic_1():
     OUTPUTS = [1, 2]
     for idx, input in enumerate(INPUTS):
         assert calculate_heuristic_1(input) == OUTPUTS[idx]
+
+
+def test_heuristic_2():
+    """
+    test heuristic 2
+    """
+    INPUTS = [
+        "BBIJ....IJCC..IAAMGDDK.MGH.KL.GHFFL.",
+        "BBIJ....IJCCAAI..MGDDK.MGH.KL.GHFFL.",
+    ]
+    OUTPUTS = [1, 2]
+    for idx, input in enumerate(INPUTS):
+        assert calculate_heuristic_2(input) == OUTPUTS[idx]
+
+
+def test_heuristic_3():
+    """
+    test heuristic 3
+    """
+    INPUTS = [
+        "BBIJ....IJCC..IAAMGDDK.MGH.KL.GHFFL.",
+        "BBIJ....IJCCAAI..MGDDK.MGH.KL.GHFFL.",
+    ]
+    OUTPUTS = [5, 10]
+    for idx, input in enumerate(INPUTS):
+        assert calculate_heuristic_3(input) == OUTPUTS[idx]
+
+
+def test_heuristic_4():
+    """
+    test heuristic 4
+    """
+    INPUTS = [
+        "BBIJ....IJCC..IAAMGDDK.MGH.KL.GHFFL.",
+        "BBIJ....IJCCAAI..MGDDK.MGH.KL.GHFFL.",
+    ]
+    OUTPUTS = [1, 2]
+    for idx, input in enumerate(INPUTS):
+        assert calculate_heuristic_4(input) == OUTPUTS[idx]
